5b6d61312a160cb7e8deb8700230419831a5cf51
Praneeth Sarode <praneethsarode@gmail.com>
2024-09-19 23:00:03 +0530

    Fix file naming
    Signed-off-by: Praneeth Sarode <praneethsarode@gmail.com>

--- Project_2/Components/alu/alu.vhdl
+++ Project_2/Components/alu/alu.vhdl
@@ -1,6 +1,8 @@
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
+
+-- add, sub, inv_sub, mul, div, and, or, xor, inc_a, inc_b, dec_a, dec_b, not_a, not_b, neg_a, neg_b
 ENTITY alu IS
 
     PORT (
@@ -12,5 +14,27 @@ ENTITY alu IS
 END ENTITY;
 
 ARCHITECTURE behavioral OF alu IS
+    COMPONENT cla32 IS
+        PORT (
+            a, b : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+            cin : IN STD_LOGIC;
+            sum : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+            cout : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
+    COMPONENT mul32 IS
+        PORT (
+            a, b : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+            result : OUT STD_LOGIC_VECTOR(63 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    COMPONENT div32 IS
+        PORT (
+            a, b : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+            result : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+        );
+    END COMPONENT;
 BEGIN
 END ARCHITECTURE;
