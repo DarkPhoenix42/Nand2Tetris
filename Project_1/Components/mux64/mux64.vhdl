4c27fd0199b28950cb970890c03fbf1366e66acb
vibhatsu <maulikbarot2915@gmail.com>
2024-09-21 03:26:35 +0530

    mux64 modified

--- Project_1/Components/mux64/mux64.vhdl
+++ Project_1/Components/mux64/mux64.vhdl
@@ -6,11 +6,11 @@ ENTITY mux64 IS
     PORT (
         a, b : IN STD_LOGIC_VECTOR(63 DOWNTO 0);
         sel : IN STD_LOGIC;
-        o : OUT STD_LOGIC_VECTOR(63 DOWNTO 0)
+        result : OUT STD_LOGIC_VECTOR(63 DOWNTO 0)
     );
 END mux64;
 
 ARCHITECTURE behavioral OF mux64 IS
 BEGIN
-    o <= (a AND (NOT sel)) OR (b AND sel);
+    result <= (a AND (NOT sel)) OR (b AND sel);
 END Behavioral;
