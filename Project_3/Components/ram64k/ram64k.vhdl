845327bc52afac6d2618d390d73068c140058ad2
Praneeth Sarode <praneethsarode@gmail.com>
2024-10-16 15:39:14 +0530

    Add ram1k
    Signed-off-by: Praneeth Sarode <praneethsarode@gmail.com>

--- Project_3/Components/ram64k/ram64k.vhdl
+++ Project_3/Components/ram64k/ram64k.vhdl
@@ -3,34 +3,23 @@ USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
 
 ENTITY ram64k IS
-    GENERIC (
-        ram_size : INTEGER := 65536;
-        address_size : INTEGER := 16
-    );
-
     PORT (
-        clk, reset, load : IN STD_LOGIC;
-        address : IN STD_LOGIC_VECTOR(address_size - 1 DOWNTO 0);
+        clk, load, reset : IN STD_LOGIC;
+        row_address, column_address : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        unit_row_address, unit_column_address : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+
         data_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-
-        data_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+        data_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
     );
-
-END ENTITY ram64k;
+END ram64k;
 
 ARCHITECTURE ram64k_arch OF ram64k IS
-    TYPE ram_type IS ARRAY (0 TO ram_size - 1) OF STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL ram : ram_type := (OTHERS => (OTHERS => '0'));
-BEGIN
-    PROCESS (clk)
-    BEGIN
-        IF rising_edge(clk) THEN
-            IF load = '1' THEN
-                ram(to_integer(unsigned(address))) <= data_in;
-            ELSIF reset = '1' THEN
-                ram(to_integer(unsigned(address))) <= (OTHERS => '0');
-            END IF;
-            data_out <= ram(to_integer(unsigned(address)));
-        END IF;
-    END PROCESS;
-END ARCHITECTURE ram64k_arch;
+    COMPONENT ram1k IS
+        PORT (
+            clk, load, reset : IN STD_LOGIC;
+            row_address, column_address : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+            data_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+            data_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+        );
+    END COMPONENT;
+END ram64k_arch;
